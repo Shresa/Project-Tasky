34a9aec68779ef3077df2747742474e1e3f5e7d9
Shresa <shreshthasadhu@gmail.com>
2021-07-31 20:34:25 +0530

    save edit data to local storage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const taskContainer = document.querySelector(".task_container");
 
 //Global Store
-const globalStore = [];
+let globalStore = [];
 const newCard = ({
     id,
     imageUrl,
@@ -29,14 +29,14 @@ const newCard = ({
         <span class="badge bg-primary">${taskType}</span>
   </div>
   <div class="card-footer text-muted">
-    <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+    <button type="button" id=${id} class="btn btn-outline-primary float-end">Open Task</button>
   </div>
 </div>
 </div>`;
 
 
 const loadInitialTaskCards = () => {
-// access localstorage 
+  // access localstorage 
   const getInitialData = localStorage.getItem("tasky");
   if(!getInitialData) return;
 
@@ -51,13 +51,13 @@ const loadInitialTaskCards = () => {
   });
 };
 
-const updateLocalStoreage= () =>
+const updateLocalStorage= () =>
     localStorage.setItem("tasky", JSON.stringify({cards: globalStore}));
 
 const saveChanges = () => {
     const taskData = {
         id:`${Date.now()}`,
-        imageUrl: document.getElementById("imageurl").value,
+        imageUrl: document.getElementById("imageUrl").value,
         taskTitle: document.getElementById("tasktitle").value,
         taskType: document.getElementById("tasktype").value,
         taskDescription: document.getElementById("taskdescription").value,
@@ -97,7 +97,7 @@ const deleteCard = (event) => {
     return taskContainer.removeChild(
       event.target.parentNode.parentNode.parentNode.parentNode  // col-lg-4
     );
- }
+ };
 
  const editCard = (event) => {
     event = window.event;
@@ -120,6 +120,50 @@ const deleteCard = (event) => {
     taskTitle.setAttribute("contenteditable", "true");
     taskDescription.setAttribute("contenteditable", "true");
     taskType.setAttribute("contenteditable", "true");
+    submitButton.setAttribute(
+      "onclick",
+      "saveEditchanges.apply(this, arguments)"
+    );
     submitButton.innerHTML = "Save Changes";
 };
 
+const saveEditchanges = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  console.log(targetID);
+  const tagname = event.target.tagName;
+
+  let parentElement;
+
+  if (tagname === "BUTTON") {
+    parentElement = event.target.parentNode.parentNode;
+  } else {
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  const updatedData = {
+    taskTitle: taskTitle.innerHTML,
+    taskType: taskType.innerHTML,
+    taskDescription: taskDescription.innerHTML,
+  };
+
+  globalStore = globalStore.map((task) => {
+    if (task.id === targetID) {
+      return {
+        id: task.id,
+        imageUrl: task.imageUrl,
+        taskTitle: updatedData.taskTitle,
+        taskType: updatedData.taskType,
+        taskDescription: updatedData.taskDescription,
+      };
+    }
+    return task; // Important
+  });
+
+  updateLocalStorage();
+};
